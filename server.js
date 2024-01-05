805303cb729dba1cf39e4602d6a0e6088696595b
MahendarJ <mahendarj10@gmail.com>
2024-01-05 20:17:32 +0530

    login completed

--- server.js
+++ server.js
@@ -46,30 +46,70 @@ app.post("/checkout", async (req, res) => {
   );
 });
 
-app.post("/", async (req, res) => {
-  const { id, title, price } = req.body;
+const stockCount = async () => {
+  const response = await db.query("SELECT COUNT(*) FROM stock");
+  const rowCount = parseInt(response.rows[0].count);
+  return rowCount;
+};
+
+app.post("/store", async (req, res) => {
   try {
-    await db.query("INSERT INTO stock(id,title,price) VALUES ($1,$2,$3)", [
+    const { id, title, price } = req.body;
+    if (!id || !title || !price) {
+      return res.status(400).json({ message: "Invalid input data" });
+    }
+    await db.query("INSERT INTO stock(id, title, price) VALUES ($1, $2, $3)", [
       id,
       title,
       price,
     ]);
-    const response = await db.query("SELECT * FROM stock");
-    res.status(200).json({ message: response.rows.length });
+    const count = await stockCount();
+    res
+      .status(201)
+      .json({ message: "Stock item created successfully", count: count });
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    console.error(error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
-app.get("/", async (req, res) => {
+app.get("/store", async (req, res) => {
   try {
     const response = await db.query("SELECT * FROM stock");
     res.status(200).json(response.rows);
   } catch (error) {
-    res.status(400).json({ message: error.message });
+    res.status(500).json({ message: error.message });
+  }
+});
+app.delete("/store/:id", async (req, res) => {
+  try {
+    const { id } = req.params;
+    await db.query("DELETE FROM stock WHERE id=$1", [id]);
+    const count = await stockCount();
+    res
+      .status(201)
+      .json({ message: "Stock item deleted successfully", count: count });
+  } catch (error) {
+    res.status(500).json({ message: error.message });
   }
 });
 
+app.post("/", async (req, res) => {
+  const { username, password } = req.body;
+  try {
+    const response = await db.query("SELECT id,username,password FROM users");
+    const user = response.rows.filter(
+      (item) => item.username === username && item.password === password
+    );
+    if (!user) {
+      res.status(401).json({ user: "unauthenticated" });
+    } else {
+      res.status(200).json({ user: user[0]?.username, token: user[0]?.id });
+    }
+  } catch (error) {
+    res.status(500).json({ message: error.message });
+  }
+});
 app.listen(PORT, () => {
   console.log(`Running on http://localhost:${PORT}`);
 });
